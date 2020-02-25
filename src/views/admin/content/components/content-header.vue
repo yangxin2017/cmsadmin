b9d00ba7438f2455d1ee137d045f4f34f8fb4762
杨鑫 <yangxin@yangxindeMacBook-Pro.local>
2020-02-25 14:06:39 +0800

    add

---
+++ src/views/admin/content/components/content-header.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="hd-content">
+    <div class="lf-button">
+        <el-button type="primary" icon="el-icon-upload2">批量上传</el-button>
+        <el-button type="danger" icon="el-icon-delete">批量删除</el-button>
+        <el-button type="primary" icon="el-icon-check">批量审核</el-button>
+    </div>
+
+    <div class="rg-content">
+        <el-checkbox-group v-model="checkList" style="display:inline-block;">
+            <el-checkbox label="草稿"></el-checkbox>
+            <el-checkbox label="待审核"></el-checkbox>
+            <el-checkbox label="已发布"></el-checkbox>
+        </el-checkbox-group>
+
+        <el-input placeholder="请输入内容" v-model="searchKey" style="width:200px;margin-left:20px;">
+            <template slot="append"><el-button slot="append" icon="el-icon-search"></el-button></template>
+        </el-input>
+    </div>
+</div>    
+</template>
+<script>
+export default {
+    data(){
+        return {
+            checkList: [],
+            searchKey: ''
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.hd-content{
+    display:flex;
+    justify-content:space-between;
+    .rg-content{
+        display:inline-block;
+    }
+}
+</style>
---
+++ src/views/admin/content/list/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="cms-container">
+        <el-card class="box-card">
+            <content-header></content-header>
+        </el-card>
+    </div>    
+</template>
+<script>
+
+import contentHeader from '@/views/admin/content/components/content-header'
+
+export default {
+    components: {
+        'content-header': contentHeader
+    },
+    data(){
+        return {
+            content: ''
+        }
+    },
+    mounted(){
+        this.content = "Content"
+    }
+}
+</script>
+<style lang="scss" scoped>
+.cms-container{
+    padding:15px;
+}
+</style>
